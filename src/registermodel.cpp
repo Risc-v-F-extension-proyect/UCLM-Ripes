8ed95093c16069fc04b00092ff0785c833df0aae
rumanoLoko <pasatpetruvlad@gmail.com>
2026-05-22 15:27:48 +0200

    Revert "Remodificación de la UI en la versión uniciclo extended tras eliminar el muted pf_reg_wr_src" Reason: necesitamos el diseño anterior de la version extended del uniciclo tras regresar fp_reg_wr_src en el diseño. This reverts commit 9c28f1147e1f53973006d3b022b59686a191aebf.

--- src/registermodel.cpp
+++ src/registermodel.cpp
@@ -116,8 +116,7 @@ QVariant RegisterModel::data(const QModelIndex &index, int role) const {
     case Qt::FontRole:
       return QFont(Fonts::monospace, 11);
     case Qt::ForegroundRole:
-      //return QBrush(Qt::blue);
-      return QBrush(Qt::red);
+      return QBrush(Qt::blue);
     case Qt::EditRole:
       return QVariant::fromValue(registerData(idx));
     default:
