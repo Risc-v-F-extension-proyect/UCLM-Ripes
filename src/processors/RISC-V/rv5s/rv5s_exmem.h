204ed49b0ec05e4b303c0891b67aede9386d574f
rumanoLoko <pasatpetruvlad@gmail.com>
2026-05-28 11:47:52 +0200

    Definicón de registros para el pilepile EX/MEM. Refs #14

--- src/processors/RISC-V/rv5s/rv5s_exmem.h
+++ src/processors/RISC-V/rv5s/rv5s_exmem.h
@@ -16,10 +16,21 @@ class RV5S_EXMEM : public EXMEM<XLEN> {
 public:
   RV5S_EXMEM(const std::string &name, SimComponent *parent)
       : EXMEM<XLEN>(name, parent) {
+    CONNECT_REGISTERED_CLEN_INPUT(f_r2, this->clear, this->enable);
+    CONNECT_REGISTERED_CLEN_INPUT(falures, this->clear, this->enable);
+    CONNECT_REGISTERED_CLEN_INPUT(fp_reg_do_write, this->clear, this->enable);
+    CONNECT_REGISTERED_CLEN_INPUT(data_mem_wr_src_ctrl, this->clear,
+                                  this->enable);
+
     // We want stalling info to persist through clearing of the register, so
     // stalled register is always enabled and never cleared.
     CONNECT_REGISTERED_CLEN_INPUT(stalled, 0, 1);
   }
+
+  REGISTERED_CLEN_INPUT(f_r2, XLEN);
+  REGISTERED_CLEN_INPUT(falures, XLEN);
+  REGISTERED_CLEN_INPUT(fp_reg_do_write, 1);
+  REGISTERED_CLEN_INPUT(data_mem_wr_src_ctrl, enumBitWidth<DataMemWrSrc>());
 
   REGISTERED_CLEN_INPUT(stalled, 1);
 };
